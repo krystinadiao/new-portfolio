b011b4006cd3e7e698b5aba279912ffaece1bc59
Krystina <krystinadiao@gmail.com>
2016-08-28 16:36:43 -0500

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+$('#main-container').mousemove(function(e){
+    var amountMovedX = (e.pageX * -1 / 6);
+    var amountMovedY = (e.pageY * -1 / 6);
+    $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
+});
